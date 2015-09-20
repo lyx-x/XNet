9b9e55ae7321436835fe6ff359cb9dc48370a813
lyx-x <li.yuxiang.nj@gmail.com>
2015-09-20 20:17:34 +0200

    Add global variables

---
+++ src/utils/global.cpp
@@ -0,0 +1,14 @@
+/*
+ * global.cpp
+ *
+ *  Created on: Sep 20, 2015
+ *      Author: lyx
+ */
+
+#include "global.h"
+
+namespace global {
+
+
+
+} /* namespace global */
---
+++ src/utils/global.h
@@ -0,0 +1,17 @@
+/*
+ * global.h
+ *
+ *  Created on: Sep 20, 2015
+ *      Author: lyx
+ */
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+namespace global {
+
+cudnnHandle_t cudnnHandle;
+cublasHandle_t cublasHandle;
+
+} /* namespace global */
+#endif /* GLOBAL_H_ */
