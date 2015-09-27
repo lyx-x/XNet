5c19fbbc2eef3b30c0dbed3abff93da0a5df6244
lyx-x <li.yuxiang.nj@gmail.com>
2015-09-28 00:20:18 +0200

    Add comments

--- src/model/network.h
+++ src/model/network.h
@@ -26,29 +26,29 @@ namespace model {
 
 class Network {
 private:
-	std::vector<Layer*> layers;
-	float* data;
-	float* h_data;
-	int data_dim;
-	float* label;
-	float* h_label;
-	int label_dim;
-	int size, batch;
+	std::vector<Layer*> layers; // list of layers
+	float* data; // input on device
+	float* h_data; // input on host
+	int data_dim; // dimension of one input
+	float* label; // label on device
+	float* h_label; // label on host
+	int label_dim; // dimension of one label (usually 1)
+	int size, batch; // whole size of data, batch size
 public:
 	Network(float* data, int data_dim, float* label, int label_dim,
 			int count, int batch);
 	virtual ~Network();
-	void Train(int iteration, float alpha);
+	void Train(int iteration, float alpha); // train the network
 	void PushInput(int c, int h, int w);
 	void PushOutput(int label_dim);
 	void PushConvolution(int c, int kernel);
 	void PushPooling(int size, int stride);
 	void PushReLU(int output_size);
 	void PushSoftmax(int output_size);
-	void Pop();
+	void Pop(); // remove last layer
 
-	void PrintGeneral();
-	void PrintData(int offset, int r, int c, int precision);
+	void PrintGeneral(); // print general information about layers
+	void PrintData(int offset, int r, int c, int precision); // print a datum
 };
 
 } /* namespace model */
